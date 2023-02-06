9c69ba0e482ce6c51edba095a08063a162b7c16b
unknown <madan.serghei@yahoo.com>
2023-02-06 20:52:27 +0200

    FEAT: signup modal styling and redirect to login modal

--- src/components/Modal/Auth/Signup.tsx
+++ src/components/Modal/Auth/Signup.tsx
@@ -3,16 +3,14 @@ import { Input, Button, Text, Flex } from '@chakra-ui/react'
 import { useSetRecoilState } from 'recoil'
 import { authModalState } from '../../../atoms/authModalAtom'
 
-type SignupProps = {
-
-}
 
 const Signup: React.FC = () => {
 
     const setAuthModalState = useSetRecoilState(authModalState)
-    const [loginForm, setLoginForm] = useState({
+    const [signUpForm, setSignUpForm] = useState({
         email: "",
-        password: ""
+        password: "",
+        confirmPassword: ""
     })
 
     const onsubmit = () => {
@@ -22,8 +20,7 @@ const Signup: React.FC = () => {
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 
-
-        setLoginForm((prev) => ({
+        setSignUpForm((prev) => ({
             ...prev,
             [event.target.name]: event.target.value
         }))
@@ -44,36 +41,42 @@ const Signup: React.FC = () => {
                     placeholder="password"
                     type="password"
                     onChange={onChange}
+                    mb={2}
+                    fontSize="10pt"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "blue.500"
+                    }}
+                    bg="gray.50"
                 />
-                {/* <Text textAlign="center" mt={2} fontSize="10pt" color="red">
-                    {formError ||
-                        FIREBASE_ERRORS[authError?.message as keyof typeof FIREBASE_ERRORS]}
-                </Text> */}
+                <Input
+                    name="confirmPassword"
+                    placeholder="confirm password"
+                    type="password"
+                    onChange={onChange}
+                    mb={2}
+                    fontSize="10pt"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "blue.500"
+                    }}
+                    bg="gray.50"
+                />
                 <Button
                     width="100%"
                     height="36px"
                     mb={2}
                     mt={2}
                     type="submit"
-                // isLoading={loading}
                 >
                     Sign Up
                 </Button>
-                {/* <Flex justifyContent="center" mb={2}>
-                    <Text fontSize="9pt" mr={1}>
-                        Forgot your password?
-                    </Text>
-                    <Text
-                        fontSize="9pt"
-                        color="blue.500"
-                        cursor="pointer"
-                        onClick={() => toggleView("resetPassword")}
-                    >
-                        Reset
-                    </Text>
-                </Flex> */}
                 <Flex fontSize="9pt" justifyContent="center">
-                    <Text mr={1}>New here?</Text>
+                    <Text mr={1}>Already a redditor?</Text>
                     <Text
                         color="blue.500"
                         fontWeight={700}
@@ -81,11 +84,11 @@ const Signup: React.FC = () => {
                         onClick={() =>
                             setAuthModalState((prev) => ({
                                 ...prev,
-                                view: "signup"
+                                view: "login"
                             }))
                         }
                     >
-                        SIGN UP
+                        Log In
                     </Text>
                 </Flex>
             </form>
